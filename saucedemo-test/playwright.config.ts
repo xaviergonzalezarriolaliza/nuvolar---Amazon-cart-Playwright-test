0cb432a3af78911956144a3a49f6f65b18a66983
X4\/¡ G <xavier.gonzalez.arriola.liza@gmail.com>
2026-03-26 10:46:35 +0100

    update

--- saucedemo-test/playwright.config.ts
+++ saucedemo-test/playwright.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30000,
-  expect: { timeout: 5000 },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
-  reporter: [
-    ['html'],
-    ['list'] // also show steps in console
-  ],
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
   use: {
-    launchOptions: {
-                      slowMo: 2000,   // delay between actions
-                    },
-    actionTimeout: 0,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',   // keep video on failure
+    video: 'retain-on-failure',
   },
+
+  // 👇 Add this block to define the browsers you want to test
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
 });
